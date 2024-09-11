af3d4c89a0182057e45e6008b48e1dd6c4a52a2d
Donovan Odom <dodomdev@gmail.com>
2024-09-11 09:21:39 -0700

    updated styling for text wrap

--- app/algorithms/Algorithms.tsx
+++ app/algorithms/Algorithms.tsx
@@ -2,12 +2,20 @@ export default async function Algorithms(){
 
   const data = await fetch(process.env.NEXT_PUBLIC_API_URL + '/api/algorithms')
   const algorithms = await data.json()
-  const arr = algorithms.algorithms
-  console.log(arr)
+
+  function convertUnicode(input: any) {
+    return input.replace(/\\+u([0-9a-fA-F]{4})/g, (a: any,b: any) =>
+      String.fromCharCode(parseInt(b, 16)));
+  }
+
   return (
     <div className="grid gap-8 lg:grid-cols-2 grid-cols-1">
-      {arr.map((algorithm: any) => (
-        <h1>{algorithm.title}</h1>
+      {algorithms.length && algorithms.map((algorithm: any) => (
+        <div className="[&_pre]:whitespace-pre-wrap">
+          <h1 className="mb-4 text-2xl font-extrabold leading-none tracking-tight md:text-3xl lg:text-4xl">{algorithm.title}</h1>
+          <div dangerouslySetInnerHTML={{__html: algorithm.content}}/>
+          <code>{convertUnicode(algorithm.code)}</code>
+        </div>
       ))}
     </div>
   )
