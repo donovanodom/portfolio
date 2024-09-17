6d06b8e357602271dde67ee5dfb8da97ce9e5b7c
Donovan Odom <dodomdev@gmail.com>
2024-09-17 09:22:52 -0700

    u

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,6 @@ export default async function AboutMe(){
         Strategic, resourceful, results-driven technologist offering expertise in creating user-friendly and responsive frontend web applications, converting design mockups and user requirements into clean, effective code to improve user experience, optimizing web performance, executing best practices, and spearheading cross-browser compatibility. Innovative business partner, effective communicator, and problem-solving project manager known for cultivating and maintaining positive, productive relationships with designers, backend developers, and stakeholders to provide quality code and solutions that meet project goals and deadlines, championing projects to completion. Collaborative leader and team contributor with demonstrated success in partnering with diverse, cross-functional teams to stay current with the latest technologies and best practices in front-end development to drive innovation, maintain a competitive edge, and attain company goals.
         </p>
       </div>
-
       <div className="pointer">
         <Link href={'/mini-apps/' + miniApp._id}>
         <h1 className='mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl'>Featured</h1>
