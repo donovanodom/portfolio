3efc664afbdd8e180223564d8ca7603e025931c1
Donovan Odom <dodomdev@gmail.com>
2024-09-19 11:51:24 -0700

    updated stlying for text color

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ async function AboutMe(){
   return (
     <div className="grid gap-8 lg:grid-cols-2 sm:grid-cols-1">
       <div>
-        <h1 className='mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl'>FullStack, Frontend-focused, TypeScript, React, NextJS and Ruby on Rails Software Engineer.</h1>
+        <h1 className='mb-4 text-4xl font-extrabold leading-none tracking-tight text-black md:text-5xl lg:text-6xl'>FullStack, Frontend-focused, TypeScript, React, NextJS and Ruby on Rails Software Engineer.</h1>
         <br/>
         <p className='mb-6 font-normal text-gray-500'>
         Strategic, resourceful, results-driven technologist offering expertise in creating user-friendly and responsive frontend web applications, converting design mockups and user requirements into clean, effective code to improve user experience, optimizing web performance, executing best practices, and spearheading cross-browser compatibility. Innovative business partner, effective communicator, and problem-solving project manager known for cultivating and maintaining positive, productive relationships with designers, backend developers, and stakeholders to provide quality code and solutions that meet project goals and deadlines, championing projects to completion. Collaborative leader and team contributor with demonstrated success in partnering with diverse, cross-functional teams to stay current with the latest technologies and best practices in front-end development to drive innovation, maintain a competitive edge, and attain company goals.
@@ -22,17 +22,17 @@ async function AboutMe(){
         {miniApp?._id && ( 
           <>
             <Link href={'/miniApps/' + miniApp._id}>
-            <h1 className='mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl'>Featured</h1>
+            <h1 className='text-black mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl lg:text-6xl'>Featured</h1>
             <div key={miniApp._id} className="[&_pre]:whitespace-pre-wrap overflow-hidden max-h-full p-2 lg:p-0">
-                <h1 className="hover:text-blue-500 mb-4 text-xl font-extrabold leading-none tracking-tight md:text-2xl">{miniApp.title}</h1>
+                <h1 className="text-black hover:text-blue-500 mb-4 text-xl font-extrabold leading-none tracking-tight md:text-2xl">{miniApp.title}</h1>
                 <div className='text-gray-500'>
-                <img src={miniApp.image}/>
-                <br/>
-              </div>
+                  <img src={miniApp.image}/>
+                  <br/>
+                </div>
             </div>
             </Link>
-            <div>Link to repository:  &nbsp; <a className="cursor-default hover:text-blue-600 text-blue-500 hover:underline" href={miniApp['github-link']}>{miniApp['github-link']}</a></div>
-            <div className="inline">Visit site: &nbsp;
+            <div className="text-black">Link to repository:  &nbsp; <a className="cursor-default hover:text-blue-600 text-blue-500 hover:underline" href={miniApp['github-link']}>{miniApp['github-link']}</a></div>
+            <div className="text-black inline">Visit site: &nbsp;
               <a href={miniApp.site} className=" cursor-default hover:underline inline hover:text-blue-600 text-blue-500">{miniApp.site}</a>
             </div>
           </>
