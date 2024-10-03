7a0e4d38706018acdeac2cdf1639b281c4595972
Donovan Odom <dodomdev@gmail.com>
2024-10-02 19:50:04 -0700

    interativity for algorithm tags

--- app/components/reusable/Tags.tsx
+++ app/components/reusable/Tags.tsx
@@ -15,15 +15,15 @@ const Tags = ({tags}: TagsProps) => {
     setToggleTags(() => !toggleTags)
   }
   return (
-    <div className='fixed z-10 top-16 w-full ml-[-16px] md:ml-[-48px] md:top-14 mb-4' >
-      <div className='p-3 md:pt-0 bg-white border-b border-b-black'>
+    <div className={`fixed ${toggleTags ? 'z-[3]' : 'z-[5]'} top-16 w-full ml-[-16px] md:ml-[-48px] md:top-14 mb-4`}>
+      <div className={`${toggleTags ? 'p-3' : 'p-0'} max-h-44 overflow-y-scroll bg-white  ${toggleTags ? 'border-b border-b-black' : ''}`}>
         {tags.length && toggleTags && tags.map((tag: Tag) => {
           const displayTag = `${tag.name} (${tag.count})`
           return <Tag tag={displayTag}/>
         })}
       </div>
-      <div className='w-24 rounded-b-xl border-b border-l border-r border-gray-500 bg-white cursor-pointer text-sm font-normal text-center relative ml-6 md:ml-14 lg:ml-12' onClick={handleToggle}>
-        <div className='bg-white h-[1px] w-[94px] relative top-[-1px]'></div>
+      <div className={ `w-24 rounded-b-xl border-b border-l border-r border-gray-500 bg-white cursor-pointer text-sm font-normal text-center relative ${toggleTags ? 'top-0' : 'top-2'} md:top-0 ml-6 md:ml-14 lg:ml-12`} onClick={handleToggle}>
+        <div className='z-30 bg-white h-[1px] w-[94px] relative top-[-1px]'></div>
         tags
       </div>
     </div>
