fa1eb07c2c36b8c7f68556df31e643994eacee55
Donovan Odom <dodomdev@gmail.com>
2024-10-03 21:03:17 -0700

    functionality for tag based filtering on algorithms

--- app/components/reusable/Tags.tsx
+++ app/components/reusable/Tags.tsx
@@ -21,7 +21,7 @@ const Tags: ForwardRefRenderFunction <HTMLDivElement,  TagsProps> = ({tags, togg
         })}
       </div>
       <div className={ `w-24 rounded-b-xl border-b border-l border-r border-gray-500 bg-white cursor-pointer text-sm font-normal text-center relative ${toggleTags ? 'top-0' : 'top-2'} md:top-0 ml-6 md:ml-14 lg:ml-12`} onClick={handleToggle}>
-        <div className='z-30 bg-white text-black h-[1px] w-[94px] relative top-[-1px]'></div>
+        <div className='bg-white text-black h-[1px] w-[94px] relative top-[-1px]'></div>
         tags
       </div>
     </div>
